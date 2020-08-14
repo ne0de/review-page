97fe4a6c192af6083b747adff90d0d62deffa6e9
Manuel Cabral <cabral.manuel@ymail.com>
2020-08-14 18:37:35 -0300

    alerts

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,21 +1,27 @@
 function registerNewAccount(){
-    let newUser = {
-        nickname: $("#InputNick").val(),
+    const newUser = {
+        nickname:  $("#InputNick").val(),
         password: $("#inputPassword").val()
-    };
+    }
 
+    if(!newUser.nickname)
+        return alert('No pusiste un nick');
+    
     $.ajax({
         url: 'http://localhost:3000/user/',
         type: 'POST',
         data: newUser,
+        error: result => {
+            alert(result.responseText);
+        },
         success: result => {
+            alert(result);
             window.location = '/user/login';
         }
     });
 }
 
 $(document).ready(function() {
-
     $('#crearCuenta').on('submit', event => {
         event.preventDefault();
         registerNewAccount();
