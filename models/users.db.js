bdad3c36bc7250382a91f7ecfd70be2a3ac4ad46
Manuel Cabral <cabral.manuel@ymail.com>
2020-08-17 04:58:26 -0300

    new colums for user dbv

--- models/users.db.js
+++ models/users.db.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, Sequelize) => {
             nickname: Sequelize.STRING,
             password: Sequelize.STRING,
             email: Sequelize.STRING,
+            description: Sequelize.STRING,
+            avatar: {
+                type: Sequelize.STRING,
+                defaultValue: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_568656.png&f=1&nofb=1"   
+            }
         },
         {
             freezeTableName: true
