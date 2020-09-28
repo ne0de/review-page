6625ecca2920345d54fccad5cbae7843064ef14f
BelenV1927 <49540439+BelenV1927@users.noreply.github.com>
2020-09-28 02:09:49 -0300

    Se agrego la parte del FAQ

--- routes/games.js
+++ routes/games.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 const controller = require("../controllers/games.controllers");
 
-router.post('/', controller.addGame);
+router.post('/', controller.addGame); 
 
-router.get('/all', controller.mostrarTodo)
+router.get('/all', controller.mostrarTodo) 
 
-router.get('/insert', controller.showCreate);
+router.get('/insert', controller.showCreate); 
 
 module.exports = router;
