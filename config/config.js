bc327deaf96493054fb6fdc87d688ab6819a95f2
Manuel Cabral <nani.andres@hotmail.com.ar>
2020-09-29 18:08:46 -0300

    he

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "sys",
+  HOST: "us-cdbr-east-02.cleardb.com",
+  USER: "b42eddd980ffbc",
+  PASSWORD: "c3d3812a",
+  DB: "heroku_1e0e22a82821a56",
   DIALECT: "mysql"
 };
